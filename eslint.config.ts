3d9c8a6a69f15c5a20a52630caec71b4b6d4f2dc
Sergei <name.diesel@gmail.com>
2026-03-12 07:55:05 +0300

    Sprint 2 tests fixes

---
+++ eslint.config.ts
@@ -0,0 +1,47 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+    globalIgnores(['dist']),
+    {
+        files: ['src/**/*.{ts,tsx}'],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+        ],
+        rules: {
+            "semi": ["error", "always"],
+            "@typescript-eslint/no-this-alias": "off",
+            "@typescript-eslint/no-unused-vars": "off",
+            "import/order": [
+                "error",
+                {
+                    groups: [
+                        "builtin",
+                        "external",
+                        "internal",
+                        "parent",
+                        "sibling",
+                        "index",
+                        "object",
+                        "type",
+                    ],
+                    "newlines-between": "always",
+                    alphabetize: { order: "asc", caseInsensitive: true },
+                    pathGroupsExcludedImportTypes: [],
+                },
+            ],
+        },
+        settings:{
+            "import/resolver": {
+                "typescript": true,
+            },
+        },
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+    },
+])
